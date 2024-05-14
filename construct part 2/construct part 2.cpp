7dcd37871c13f252bd67b449a3fdb7b0e0fee8a0
AQA'S LAPTOP <AQA'S LAPTOP@LAPTOP-AA3N1G6J>
2024-05-14 11:01:48 +0700

    15

--- construct part 2/construct part 2.cpp
+++ construct part 2/construct part 2.cpp
@@ -16,7 +16,9 @@ public:
 	static void setNim(int pNim) { nim = pNim; /*Definisi Function"*/ }
 	static int getNim() { return nim; /*Definisi Function*/ }
 
-	mahasiswa(string pnama) :nama(pnama) {	setID();	}
+	Mahasiswa(string pnama) :nama(pnama) {
+		setID();
+	}
 };
 
 int Mahasiswa::nim = 0;
