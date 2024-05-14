e9b5790a0f451cff8127034d3f676acfb3c25e5e
AQA'S LAPTOP <AQA'S LAPTOP@LAPTOP-AA3N1G6J>
2024-05-14 11:29:34 +0700

    17

---
+++ Constructp3/Constructp3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa {
+private:
+	int nim;
+	string nama;
+public:
+	Mahasiswa();
+	Mahasiswa(int);
+	Mahasiswa(string);
+	Mahasiswa(int iNim, string iNama);
+	void cetak();
+};
+
+Mahasiswa::Mahasiswa() {
+	nim = 161;
+	nama = "";
+}
+Mahasiswa::Mahasiswa(int iNim) {
+	nim = iNim;
+}
+
+Mahasiswa::Mahasiswa(string iNama) {
+	nim = 161;
+	nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
+void Mahasiswa::cetak() {
+	cout << endl << "Nim =" << nim << endl;
+	cout << " Nama = " << nama << endl;
+}
+
+int main() {
+
+}
