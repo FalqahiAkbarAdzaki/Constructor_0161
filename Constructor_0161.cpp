01582c34261ba12cad38df2c95597182f5b1321f
AQA'S LAPTOP <AQA'S LAPTOP@LAPTOP-AA3N1G6J>
2024-05-14 10:25:50 +0700

    7

--- Constructor_0161.cpp
+++ Constructor_0161.cpp
@@ -17,9 +17,16 @@ public:
 	}
 };
 
-int Mahasiswa : nim = 161;
+int Mahasiswa::nim = 161;
 
-void Mahasiswa : setID() {
+void Mahasiswa::setID() {
 	id = ++nim;
 }
 
+void Mahasiswa::printAll(){
+	cout << "ID =" << id << endl;
+	cout << "Nama =" << nama << endl;
+	cout << endl;
+}
+
+int m
